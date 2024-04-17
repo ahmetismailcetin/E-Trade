8ac761a055cf472c72617abd83a5332286a12883
konuk <konuk@301-00010>
2024-04-17 20:05:55 +0300

    Adding Nuget Package(Sqlserver.Tools,Identity) Entities were created,database connection was made and migration was started

---
+++ E-Trade.Data/Context/E-TradeContext.cs
@@ -0,0 +1,20 @@
+﻿using E_Trade.Data.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Data.Context
+{
+    public class E_TradeContext : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Localhost;Database=ETradeDb;User Id=sa;Password=1234;TrustServerCertificate=true;");
+        }
+    }
+}
